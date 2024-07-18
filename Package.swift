ac2456a8c54b448deb5ef44e48d8a2b265b3b318
Sunny Fugate <sunny.fugate@gmail.com>
2024-07-17 17:39:55 -0700

    Update Package.swift to allow builds for iOS 15 and Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SunCalc",
     defaultLocalization: "en",
-	platforms: [.iOS(.v16), .macCatalyst(.v16), .macOS(.v13)],
+	platforms: [.iOS(.v15), .macCatalyst(.v16), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
